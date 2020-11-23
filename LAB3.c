ae019e8a771b3a87271e5af4bdf2f1339f2a3047
YaroslavLys <mryaroslav337@gmail.com>
2020-11-23 09:58:41 +0200

    my first commit

---
+++ LAB3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int func(int a) {
+    int k = 0;
+    while (a != 0) {
+        a /= 10;
+        k++;
+    }
+    return a;
+}
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    printf("%d", func(n));
+    return 0;
+}
